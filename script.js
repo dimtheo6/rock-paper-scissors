7e767ba1100431037487efd56945fad89b19b601
Dimitris Theodosopoulos <155730305+dimtheo6@users.noreply.github.com>
2024-02-29 18:55:30 +0200

    created the playRound function with all the winning conditions

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function getComputerChoice(computerChoice) {
 
     computerChoice = choice[Math.floor(Math.random() * choice.length)];
     console.log(computerChoice);
+    return computerChoice;
+
 }
 
 function getPlayerChoice(playerChoice) {
@@ -12,8 +14,44 @@ function getPlayerChoice(playerChoice) {
     while (playerChoice !== `rock` && playerChoice !== `paper` && playerChoice !== `scissors`) {
         playerChoice = prompt(`Please choose "Rock, paper or scissors"`).toLowerCase();
     }
-    console.log(playerChoice);
+    return playerChoice;
 }
 
-getPlayerChoice();
+function playRound(){
+
+    computerChoice = getComputerChoice();
+    playerChoice = getPlayerChoice();
+
+    if (computerChoice === `rock` && playerChoice === `rock`){
+        console.log('its a tie');
+    }else 
+    if(computerChoice === `rock` && playerChoice === `paper`){
+        console.log(`You Win! Paper beats Rock.`);
+    }else
+    if(computerChoice === `rock` && playerChoice === `scissors`){
+        console.log(`You Lose! Rock beats Scissors.`);
+    }else
+    if(computerChoice === `paper` && playerChoice === `paper`){
+        console.log(`Its a tie.`);
+    }else
+    if(computerChoice === `paper` && playerChoice === `rock`){
+        console.log(`You Lose! Paper beats Rock.`);
+    }else
+    if(computerChoice === `paper` && playerChoice === `scissors`){
+        console.log(`You Win! Scissors beats Paper.`);
+    }else
+    if(computerChoice === `scissors` && playerChoice === `scissors`){
+        console.log(`Its a tie.`);
+    }else
+    if(computerChoice === `scissors` && playerChoice === `rock`){
+        console.log(`You Win! Rock beats Scissors.`);
+    }else
+    if(computerChoice === `scissors` && playerChoice === `paper`){
+        console.log(`You Lose! Scissors beats Paper.`);
+    }
+}
+
+playRound();
+
+
 
