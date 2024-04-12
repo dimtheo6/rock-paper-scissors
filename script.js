61c1640dc81e4a967a816948ec23ebf135355184
Dimitris Theodosopoulos <155730305+dimtheo6@users.noreply.github.com>
2024-04-12 08:30:53 +0300

    remade the js to make the game playable using buttons

--- script.js
+++ script.js
@@ -1,5 +1,102 @@
 let computerScore = 0;
 let playerScore = 0;
+let rounds = 0;
+
+function game(){
+
+
+function playGame(){
+    const btnRock = document.querySelector('#rock')
+    const btnPaper = document.querySelector('#paper')
+    const btnScissors = document.querySelector('#scissors')
+    const display = document.querySelector('#display')
+    const computerOptions = [`rock`, `paper`, `scissors`];
+
+    let playerChoice;
+    let computerChoice;
+
+    let i=1;
+ 
+    btnRock.addEventListener('click',()=>{
+        console.log(`~~~~ROUND ${i}~~~~`);
+        playerChoice = 'rock';
+        display.textContent = playerChoice;
+        computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+        console.log(computerChoice);
+        winner(playerChoice,computerChoice);
+        i++
+    })
+    
+    btnPaper.addEventListener('click',()=>{
+        console.log(`~~~~ROUND ${i}~~~~`);
+        playerChoice = 'paper';
+        display.textContent = playerChoice;
+        computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+        console.log(computerChoice);
+        winner(playerChoice,computerChoice);
+        i++
+    })
+    
+    btnScissors.addEventListener('click',()=>{
+        console.log(`~~~~ROUND ${i}~~~~`);
+        playerChoice = 'scissors';
+        display.textContent = playerChoice;
+        computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+        console.log(computerChoice);
+        winner(playerChoice,computerChoice);
+        i++
+    })
+
+} 
+
+const winner = (playerChoice,computerChoice)=>{
+
+
+    if (computerChoice === playerChoice) {
+        console.log('its a tie');
+    } else
+        if (computerChoice === `rock` && playerChoice === `paper`) {
+            console.log(`You Win! Paper beats Rock.`);
+            playerScore = playerScore + 1;
+        } else
+            if (computerChoice === `rock` && playerChoice === `scissors`) {
+                console.log(`You Lose! Rock beats Scissors.`);
+                computerScore = computerScore + 1;
+            } else
+                if (computerChoice === `paper` && playerChoice === `rock`) {
+                    console.log(`You Lose! Paper beats Rock.`);
+                    computerScore = computerScore + 1;
+                } else
+                    if (computerChoice === `paper` && playerChoice === `scissors`) {
+                        console.log(`You Win! Scissors beats Paper.`);
+                        playerScore = playerScore + 1;
+                    } else
+                        if (computerChoice === `scissors` && playerChoice === `rock`) {
+                            console.log(`You Win! Rock beats Scissors.`);
+                            playerScore = playerScore + 1;
+                        } else
+                            if (computerChoice === `scissors` && playerChoice === `paper`) {
+                                console.log(`You Lose! Scissors beats Paper.`);
+                                computerScore = computerScore + 1;
+                            }
+                            console.log(`computer score is : `, computerScore);
+                            console.log(`player score is : `, playerScore);
+
+                            if (playerScore === 5){
+                                console.log('the player won!!');
+                            }else if(computerScore === 5){
+                                console.log('the computer won!!');
+                            }
+}
+
+
+playGame();
+}
+
+game();
+
+/* let computerScore = 0;
+let playerScore = 0;
 
 function getComputerChoice(computerChoice) {
     let choice = [`rock`, `paper`, `scissors`];
@@ -74,5 +171,4 @@ for (i = 1; i <= 5; i++) {
     gameStart();
 }
 
-winner();
-
+*/
